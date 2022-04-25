565ef186710e01d46259fd1d292a99aa00666a3d
Andy <flitexl718@gmail.com>
2022-04-25 22:26:45 +0300

    Init: create project base

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslocoRootModule } from './transloco-root.module';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HttpClientModule, TranslocoRootModule, StoreModule.forRoot(reducers, {
+      metaReducers
+    }), StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
