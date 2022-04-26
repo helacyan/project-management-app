e4ff9716bcb81d1a6f5c07de02accfdc36d7fc82
Iryna Bakhur <ira.bakhur@icloud.com>
2022-04-26 18:31:32 +0300

    feat: implement husky+lint-staged; fix: update prettier config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { HttpClientModule } from '@angular/common/http'
-import { TranslocoRootModule } from './transloco-root.module'
-import { StoreModule } from '@ngrx/store'
-import { reducers, metaReducers } from './reducers'
-import { StoreDevtoolsModule } from '@ngrx/store-devtools'
-import { environment } from '../environments/environment'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslocoRootModule } from './transloco-root.module';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/transloco-root.module.ts
+++ src/app/transloco-root.module.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 import {
   TRANSLOCO_LOADER,
   Translation,
@@ -6,16 +6,16 @@ import {
   TRANSLOCO_CONFIG,
   translocoConfig,
   TranslocoModule,
-} from '@ngneat/transloco'
-import { Injectable, NgModule } from '@angular/core'
-import { environment } from '../environments/environment'
+} from '@ngneat/transloco';
+import { Injectable, NgModule } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader implements TranslocoLoader {
   constructor(private http: HttpClient) {}
 
   getTranslation(lang: string) {
-    return this.http.get<Translation>(`/assets/i18n/${lang}.json`)
+    return this.http.get<Translation>(`/assets/i18n/${lang}.json`);
   }
 }
 
