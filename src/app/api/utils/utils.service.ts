efb77d2e13979e9856b1ecc37f7aef9c160d2a46
Andy <flitexl718@gmail.com>
2022-04-29 14:49:10 +0300

    refactor: remove useless expressions

--- src/app/api/utils/utils.service.ts
+++ src/app/api/utils/utils.service.ts
@@ -8,14 +8,7 @@ export class UtilsService {
     localStorage.setItem('userInfo', JSON.stringify(token));
   }
 
-  getLocalStorage = (): string | null => {
-    const userInfoObj: string | null = localStorage.getItem('userInfo');
-    if (userInfoObj) return userInfoObj;
-    return null;
-  };
-
   getTokenFromStorage = () => {
-    const res: string | null = this.getLocalStorage();
-    if (res) return JSON.parse(res);
+    return localStorage.getItem('userInfo');
   };
 }
