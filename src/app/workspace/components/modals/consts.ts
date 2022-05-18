b37ccc3356270fced10f591a52cec55262f1709a
weronika-fed <nikafed3@gmail.com>
2022-05-18 17:18:59 +0300

    refactor: header

---
+++ src/app/workspace/components/modals/consts.ts
@@ -0,0 +1,12 @@
+export const TITLE_ERRORS_MESSAGES = {
+  required: 'Пожалуйста, введите название',
+  minLength: 'Название слишком короткое',
+  maxLength: 'Название слишком длинное',
+  hashSymbol: 'Название не может содержать символ "#"',
+};
+
+export const DESCRIPTION_ERRORS_MESSAGES = {
+  required: 'Пожалуйста, введите описание',
+  minLength: 'Описание слишком короткое',
+  maxLength: 'Описание слишком длинное',
+};
